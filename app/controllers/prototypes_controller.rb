0bf2fb68807a6b1d6e0e6f30e6e9b726d0382e6b
szakisip <ssg2012.yamato.s@gmail.com>
2021-10-17 12:47:51 +0900

    プロトタイプ関連実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,48 @@
 class PrototypesController < ApplicationController
   def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    # @user = User.find(params[:user_id])
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+      redirect_to prototype_path(prototype.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :image, :catch_copy, :concept).merge(user_id: current_user.id)
   end
 end
